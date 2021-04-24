ecfd81d828c5003c79265dd72edcffa826a9e7e6
Ofelia <oer.rodriguez@gmail.com>
2021-04-24 23:27:40 +0000

    Adding user model

--- model.py
+++ model.py
@@ -5,3 +5,18 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True,unique=True)
+    email = db.Column(db.Text, unique=True, nullable=False)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    created_at = db.Column((db.datetime, default=datetime.datetime.utcnow), nullable=False)
+    modified_at = db.Column((db.datetime, default=datetime.datetime.utcnow), nullable=False)
+    password = db.Column(db.Text, nullable=False)
+
+
+
