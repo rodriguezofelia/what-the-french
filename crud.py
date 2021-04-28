39daa383fb5cfa653e3aa69456bde5f529a7ba77
Ofelia <oer.rodriguez@gmail.com>
2021-04-28 03:20:52 +0000

    Created function to create and return a new user

--- crud.py
+++ crud.py
@@ -1,3 +1,14 @@
 """CRUD operations."""
 
 from model import db, User, Grade, Quiz, Quiz_Sentence, Verb, Tense, Conjugated_Verb, Sentence 
+
+def create_user(email, first_name, last_name, created_at, modified_at, password):
+    """Create and return a new user."""
+
+    user = User(email=email, first_name=first_name, last_name=last_name, created_at=created_at, modified_at=modified_at, password=password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
