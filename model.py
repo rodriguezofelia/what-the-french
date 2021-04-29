5d36cf676bae1879f827bce2f21a3ad73da88f6d
Ofelia <oer.rodriguez@gmail.com>
2021-04-29 00:55:12 +0000

    Fixing typo

--- model.py
+++ model.py
@@ -105,7 +105,7 @@ class Sentence(db.Model):
 
     conjugated_verb_id = db.relationship('Sentence', backref='sentences')
 
-def connect_to_db(flask_app, db_uri='postgresql:///wtf’, echo=True):
+def connect_to_db(flask_app, db_uri='postgresql:///wtf', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     flask_app.config['SQLALCHEMY_ECHO'] = echo
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
