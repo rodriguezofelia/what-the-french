79b7c0b072b681db56d3827444b327da12989751
Ofelia <oer.rodriguez@gmail.com>
2021-05-05 00:58:20 +0000

    Got Spotify authorization to work

---
+++ static/js/podcast.js
@@ -0,0 +1,11 @@
+console.log("ARE YOU HOOKED UP OR WHAT");
+
+fetch("https://accounts.spotify.com/api/token?grant_type=client_credentials", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization: `Basic ${btoa(
+      secrets.spotify_client_id + ":" + secrets.spotify_client_secret
+    )}`,
+  },
+});
