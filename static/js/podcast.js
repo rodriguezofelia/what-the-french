3135e8e4702c1938d4a0e43ec48d9460eb8a3b1d
Ofelia <oer.rodriguez@gmail.com>
2021-05-07 03:26:16 +0000

    Returning playlists from Spotify Search API

--- static/js/podcast.js
+++ static/js/podcast.js
@@ -1,9 +1,34 @@
-fetch("https://accounts.spotify.com/api/token?grant_type=client_credentials", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-    Authorization: `Basic ${btoa(
-      secrets.spotify_client_id + ":" + secrets.spotify_client_secret
-    )}`,
-  },
-});
+const getPodcasts = async () => {
+  fetch(
+    "https://accounts.spotify.com/api/token?grant_type=client_credentials",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${btoa(
+          secrets.spotify_client_id + ":" + secrets.spotify_client_secret
+        )}`,
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      return fetch(
+        "https://api.spotify.com/v1/search?q=french%20podcasts&type=playlist",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${response.access_token}`,
+          },
+        }
+      );
+    })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+    });
+};
+
+getPodcasts();
