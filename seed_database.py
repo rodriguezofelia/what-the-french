2a57349e1cc7ee5fb75159c0fc0f7464501cd4eb
Ofelia <oer.rodriguez@gmail.com>
2021-05-14 03:19:09 +0000

    Updating html for podcast data

--- seed_database.py
+++ seed_database.py
@@ -13,3 +13,9 @@ os.system('createdb wtf')
 
 model.connect_to_db(server.app)
 model.db.create_all()
+
+# create quizzes with sentences and answer references
+
+# create random set of users with emails and pws
+
+# create a random set of grades for users
