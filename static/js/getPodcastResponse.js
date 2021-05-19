c4595c61ebf5e98d51b36ae293f0b0bcd0c5187f
Ofelia <oer.rodriguez@gmail.com>
2021-05-19 00:53:50 +0000

    Grabbed API response and created a div with respective data.

--- static/js/getPodcastResponse.js
+++ static/js/getPodcastResponse.js
@@ -1,5 +1,5 @@
-const getPodcasts = async () => {
-  fetch(
+const requestSpotifyPlaylists = async () => {
+  return await fetch(
     "https://accounts.spotify.com/api/token?grant_type=client_credentials",
     {
       method: "POST",
@@ -15,7 +15,7 @@ const getPodcasts = async () => {
     .then((response) => {
       console.log(response);
       return fetch(
-        "https://api.spotify.com/v1/search?q=french%20podcasts&type=playlist",
+        "https://api.spotify.com/v1/search?q=french%10podcasts&type=playlist",
         {
           method: "GET",
           headers: {
@@ -28,7 +28,29 @@ const getPodcasts = async () => {
     .then((response) => response.json())
     .then((response) => {
       console.log(response);
+      return response;
     });
+};
+
+const showResponse = (response) => {
+  const podcastListContainer = document.querySelector("#podcastPlaylistList");
+  console.log("are you wroking today ", response.playlists.items);
+
+  const podcastHtmlItems = response.playlists.items.map((item) => {
+    return `<div>
+    <div>Name: ${item.name}</div>
+    <div>Description: ${item.description}</div>
+    <div>URL: ${item.external_urls.spotify}</div>
+    <div>Image: ${item.images[0].url}</div>
+    <div>Tracks: ${item.tracks.href}</div>
+    </div>`;
+  });
+  podcastListContainer.innerHTML = podcastHtmlItems.join("");
+};
+
+const getPodcasts = async () => {
+  const response = await requestSpotifyPlaylists();
+  showResponse(response);
 };
 
 getPodcasts();
